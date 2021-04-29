6ef9985abae5dd24d27021cf3e5440bceddcedf0
Abhi Motgi <apmotgi16@gmail.com>
2021-04-29 19:47:10 -0400

    fixed card bug

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import './App.css';
-import {CodeTextbox} from './components/CodeTextbox'
-import {CardList} from './components/CardList'
+import React from "react";
+import "./App.css";
+import { CodeTextbox } from "./components/CodeTextbox";
+import { CardList } from "./components/CardList";
 
 class App extends React.Component {
-
-
   constructor(props) {
-    super(props)
-    this.cardCount = 0
+    super(props);
+    this.cardCount = 0;
     this.state = {
       startIdx: 0,
       endIdx: 0,
       code: null,
-      cards: []
-    }
+      cards: [],
+    };
   }
 
   updateTextboxVars = (event) => {
@@ -22,77 +20,100 @@ class App extends React.Component {
     this.setState({
       code: value,
       startIdx: selectionStart,
-      endIdx: selectionEnd
-    })
-  }
+      endIdx: selectionEnd,
+    });
+  };
 
   addCard = () => {
     if (this.state.endIdx > this.state.startIdx) {
       let card = {
         startIdx: this.state.startIdx,
         endIdx: this.state.endIdx,
-        codeBlock: this.state.code.substring(this.state.startIdx, this.state.endIdx),
+        codeBlock: this.state.code.substring(
+          this.state.startIdx,
+          this.state.endIdx
+        ),
         heading: "# Heading",
         content: "Content",
         cardKey: this.cardCount,
-      }
-      this.setState({cards: [...this.state.cards, card]})
-      this.cardCount += 1
+      };
+      this.setState({ cards: [...this.state.cards, card] });
+      this.cardCount += 1;
+    } else {
+      console.log("invalid selection");
     }
-    else {
-      console.log('invalid selection')
-    }
-  }
+  };
 
   setSelection = (startIdx, endIdx) => {
     this.setState({
       startIdx,
-      endIdx
-    })
-  }
+      endIdx,
+    });
+  };
 
   updateCard = (element, idx, value) => {
-    let cards = this.state.cards.slice()
-    switch(element) {
+    let cards = this.state.cards.slice();
+    switch (element) {
       case "h1":
-        cards[idx].heading = value
+        cards[idx].heading = value;
         break;
       case "content":
-        cards[idx].content = value
+        cards[idx].content = value;
         break;
       default:
         break;
     }
-    this.setState({cards})
-  }
+    this.setState({ cards });
+  };
 
   deleteCard = (idx) => {
-    const cards = [...this.state.cards]
-    cards.splice(idx, 1)
+    const cards = [...this.state.cards];
+    cards.splice(idx, 1);
 
-    this.setState({cards})
-  }
+    this.setState({ cards });
+  };
 
   render() {
     return (
       <div class="container mx-auto flex flex-row">
         <div class="flex-col flex-grow w-3/4 m-2 h-3/4">
           <div class="container mx-auto">
-            <CodeTextbox startIdx={this.state.startIdx} endIdx={this.state.endIdx} updateVars={(event) => this.updateTextboxVars(event)}></CodeTextbox>
+            <CodeTextbox
+              startIdx={this.state.startIdx}
+              endIdx={this.state.endIdx}
+              updateVars={(event) => this.updateTextboxVars(event)}
+            ></CodeTextbox>
           </div>
           <div class="flex flex-row">
-            <button class="bg-green-500 text-white p-2 m-2 rounded-md" onClick={this.addCard}>Add comment</button>
-            <button class="bg-gray-800 text-white p-2 m-2 rounded-md" onClick={() => this.setState({cards:[]})}>Clear cards</button>
+            <button
+              class="bg-green-500 text-white p-2 m-2 rounded-md"
+              onClick={this.addCard}
+            >
+              Add comment
+            </button>
+            <button
+              class="bg-gray-800 text-white p-2 m-2 rounded-md"
+              onClick={() => this.setState({ cards: [] })}
+            >
+              Clear cards
+            </button>
           </div>
         </div>
         <div class="w-1/4 flex-shrink m-2">
-          <CardList cards={this.state.cards} cardClicked={(startIdx, endIdx) => this.setSelection(startIdx, endIdx)} cardUpdated={(element, idx, value) => this.updateCard(element, idx, value)} cardDeleted={(idx) => this.deleteCard(idx)}></CardList>
+          <CardList
+            cards={this.state.cards}
+            cardClicked={(startIdx, endIdx) =>
+              this.setSelection(startIdx, endIdx)
+            }
+            cardUpdated={(element, idx, value) =>
+              this.updateCard(element, idx, value)
+            }
+            cardDeleted={(idx) => this.deleteCard(idx)}
+          ></CardList>
         </div>
       </div>
     );
   }
-
 }
-  
 
 export default App;
--- src/components/CodeTextbox.js
+++ src/components/CodeTextbox.js
@@ -1,18 +1,23 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef } from "react";
 export const CodeTextbox = (props) => {
-    const textAreaRef = useRef(null)
+  const textAreaRef = useRef(null);
 
-    useEffect(() => {
-        if (textAreaRef !== null) {
-            textAreaRef.current.focus()
-            textAreaRef.current.selectionStart = props.startIdx
-            textAreaRef.current.selectionEnd = props.endIdx
-        }
-    })
+  useEffect(() => {
+    if (textAreaRef !== null) {
+      textAreaRef.current.focus();
+      textAreaRef.current.selectionStart = props.startIdx;
+      textAreaRef.current.selectionEnd = props.endIdx;
+    }
+  });
 
-    return (
-        <div>
-            <textarea class="font-mono text-sm w-full bg-gray-100 h-96 m-2 p-2 focus:bg-gray-200" ref={textAreaRef} onBlur={(event) => props.updateVars(event)} onMouseUp={(event) => props.updateVars(event)} onChange={(event) => props.updateVars(event)}></textarea>
-        </div>
-    )
-}
+  return (
+    <div>
+      <textarea
+        class="font-mono text-sm w-full bg-gray-100 h-96 m-2 p-2 focus:bg-gray-200"
+        ref={textAreaRef}
+        onMouseUp={(event) => props.updateVars(event)}
+        onChange={(event) => props.updateVars(event)}
+      ></textarea>
+    </div>
+  );
+};

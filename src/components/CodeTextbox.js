12e5363873ff2d2444b48580de65142d28b3f6a5
Abhi Motgi <apmotgi16@gmail.com>
2021-04-30 19:55:58 -0400

    added placeholder to textarea, decreased rows

--- src/components/CodeTextbox.js
+++ src/components/CodeTextbox.js
@@ -18,7 +18,8 @@ export const CodeTextbox = (props) => {
       <textarea
         class="font-mono text-sm w-full bg-gray-100 m-2 p-2  focus:outline-none"
         ref={textAreaRef}
-        rows="40"
+        rows="30"
+        placeholder="Paste your code..."
         // onMouseUp={(event) => props.updateVars(event)}
         onChange={(event) => props.updateVars(event)}
         onSelect={(event) => props.updateVars(event)}
